a1b128a60a5dbe86920f7ac8cef68f47ce5d9ce5
Gabriel Bulle De Barros <gbb1@poli.br>
2024-06-16 11:59:06 -0300

    add ex10.cpp e esqueleto do codigo

---
+++ ex10.cpp
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *   ex10.cpp                                 Version 20240616.115456      *
+ *                                                                         *
+ *   Calculadora polonesa em C++ (Pilhas)                                  *
+ *   Copyright (C) 2024         by Gabriel Bulle De Barros                 *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License.               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Gabriel Bulle De Barros                                               *
+ *   Email: gbb1@poli.br                                                   *
+ *   Webpage: http://beco.poli.br/gbb1@poli.br                             *
+ *   Phone: 81991396798                                                    *
+ ***************************************************************************/
+
+/*
+ * Instrucoes para compilar:
+ *   $ make ex10.out
+ */
+
+/* ---------------------------------------------------------------------- */
+/* includes */
+#include <iostream> /* (Input/output library) Several standard stream objects */
+
+/* ---------------------------------------------------------------------- */
+/* definitions */
+
+
+/* ---------------------------------------------------------------------- */
+/* classes : */
+
+class document
+{
+    public:
+};
+
+/* ---------------------------------------------------------------------- */
+/* methods */
+
+/* ---------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------- */
+int main(void)
+{
+    return 0;
+}
+
+/* add more functions here */
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : CPP config - Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20220718.172615      */
+
+
